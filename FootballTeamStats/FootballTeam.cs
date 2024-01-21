e447dfa05ce4f0508817745dc84e4309212b2cf0
pisiaq <lukpisiak@protonmail.com>
2024-01-21 12:36:14 +0100

    Rest of the code and preparation of the project for unit testing.

---
+++ FootballTeamStats/FootballTeam.cs
@@ -0,0 +1,35 @@
+﻿namespace FootballTeamStats
+{
+    class FootballTeam : SportsTeamBase
+    {
+        public FootballTeam(string name, int[] goals) : base(name, goals)
+        {
+        }
+
+        public char GetAverageGrade()
+        {
+            double averageGoals = GetAverageGoals();
+
+            if (averageGoals >= 4.5)
+            {
+                return 'A';
+            }
+            else if (averageGoals >= 3.5)
+            {
+                return 'B';
+            }
+            else if (averageGoals >= 2.5)
+            {
+                return 'C';
+            }
+            else if (averageGoals >= 1.5)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'E';
+            }
+        }
+    }
+}
--- FootballTeamStats/Program.cs
+++ FootballTeamStats/Program.cs
@@ -1,2 +1,59 @@
-﻿// Test
-Console.WriteLine("SEND TO GITHUB");
+﻿using FootballTeamStats;
+class  FootballTeamStatistics
+{
+    public delegate void AddTeamHandler(IScorable team);
+
+    public static event AddTeamHandler TeamAdded;
+
+    static void Main()
+    {
+        List<IScorable> teams = new List<IScorable>();
+        Console.WriteLine("Welcome to the program for displaying statistics of football teams!\n");
+        while (true)
+        {
+            
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add football team\n");
+            Console.WriteLine("2. Display statistics\n");
+            Console.WriteLine("3. Save to memory/file\n");
+            Console.WriteLine("Q. Quit the application\n");
+
+            char choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case '1':
+                    AddTeam(teams);
+                    break;
+                case '2':
+                    DataOperations.DisplayStatistics(teams);
+                    break;
+                case '3':
+                    FileOperations.SaveData(teams);
+                    break;
+                case 'Q':
+                case 'q':
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+    static void AddTeam(List<IScorable> teams)
+    {
+        Console.WriteLine("Enter the name of the football team:");
+        string teamName = Console.ReadLine();
+
+        int[] goals = DataOperations.ReadGoals();
+
+        FootballTeam newTeam = new FootballTeam(teamName, goals);
+        teams.Add(newTeam);
+
+        TeamAdded?.Invoke(newTeam);
+
+        Console.WriteLine($"Team {teamName} added with match results: {string.Join(", ", goals)}");
+        Console.WriteLine("Team added successfully!");
+    }
+}
