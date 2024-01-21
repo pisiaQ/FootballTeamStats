c7a64f6f8378d900f9cbbb3dff9007deabd86eae
pisiaq <lukpisiak@protonmail.com>
2024-01-21 12:09:22 +0100

    Interface and base class

---
+++ FootballTeamStats/IScorable.cs
@@ -0,0 +1,15 @@
+﻿namespace FootballTeamStats
+{
+    public interface IScorable
+    {
+        string Name { get; }
+        int[] Goals { get; }
+        int GetMinGoals();
+        int GetMaxGoals();
+        double GetAverageGoals();
+        int GetTotalGoals();
+        char GetAverageGrade();
+
+        event EventHandler GoalsChanged;
+    }
+}
---
+++ FootballTeamStats/SportsTeamBase.cs
@@ -0,0 +1,52 @@
+﻿namespace FootballTeamStats
+{
+   public abstract class SportsTeamBase : IScorable
+    {
+        private string name;
+        private int[] goals;
+
+        public event EventHandler GoalsChanged;
+
+        public SportsTeamBase(string name, int[] goals)
+        {
+            this.name = name;
+            this.goals = goals;
+        }
+
+        public string Name => name;
+
+        public int[] Goals => goals;
+
+        public virtual int GetMinGoals() => goals.Min();
+        public virtual int GetMaxGoals() => goals.Max();
+        public virtual double GetAverageGoals() => goals.Average();
+        public virtual int GetTotalGoals() => goals.Sum();
+
+        public char GetAverageGrade()
+        {
+            double averageGoals = GetAverageGoals();
+
+            if (averageGoals >= 4.5)
+            {
+                return 'A';
+            }
+            else if (averageGoals >= 3.5)
+            {
+                return 'B';
+            }
+            else if (averageGoals >= 2.5)
+            {
+                return 'C';
+            }
+            else if (averageGoals >= 1.5)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'E';
+            }
+        }
+    }
+
+}
